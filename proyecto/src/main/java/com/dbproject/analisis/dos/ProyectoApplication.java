e0ad7cfc184ef7a200f1d228b6072ca07fca37c4
karlapu <kpuy@miumg.edu.gt>
2025-08-25 23:08:44 -0600

    Add files via upload

---
+++ proyecto/src/main/java/com/dbproject/analisis/dos/ProyectoApplication.java
@@ -0,0 +1,14 @@
+package com.dbproject.analisis.dos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoApplication.class, args);
+	}
+
+}
+
---
+++ proyecto/src/test/java/com/dbproject/analisis/dos/ProyectoApplicationTests.java
@@ -0,0 +1,11 @@
+package com.dbproject.analisis.dos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
